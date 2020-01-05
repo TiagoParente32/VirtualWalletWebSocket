57776b31e455d062b60e9dcd6b2762c048151f07
Pedro Santos <7psantos@gmail.com>
2020-01-05 17:44:31 +0000

    send mail and show notifications fixed

--- server.js
+++ server.js
@@ -1,31 +1,15 @@
-/*jshint esversion: 6 */
-
 var app = require('http').createServer();
 
-// Se tiverem problemas com "same-origin policy" deverão activar o CORS.
-
-// Aqui, temos um exemplo de código que ativa o CORS (alterar o url base) 
-
-// var app = require('http').createServer(function(req,res){
-// Set CORS headers
-//  res.setHeader('Access-Control-Allow-Origin', 'http://---your-base-url---');
-//  res.setHeader('Access-Control-Request-Method', '*');
-//  res.setHeader('Access-Control-Allow-Methods', 'UPGRADE, OPTIONS, GET');
-//  res.setHeader('Access-Control-Allow-Credentials', true);
-//  res.setHeader('Access-Control-Allow-Headers', req.header.origin);
-//  if ( req.method === 'OPTIONS' || req.method === 'UPGRADE' ) {
-//      res.writeHead(200);
-//      res.end();
-//      return;
-//  }
-// });
-
-// NOTA: A solução correta depende da configuração do próprio servidor, 
-// e alguns casos do próprio browser.
-// Assim sendo, não se garante que a solução anterior funcione.
-// Caso não funcione é necessário procurar/investigar soluções alternativas
-
 var io = require('socket.io')(app);
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'ipleiriadad@gmail.com',
+		pass: 'zsjojgjnbfpxwdmj'
+	}
+});
 
 var LoggedUsers = require('./loggedusers.js');
 
@@ -48,41 +32,55 @@ io.on('connection', function (socket) {
 	console.log('client has connected (socket ID = '+socket.id+ ')');
 
 	socket.on("sendSocketEmailToServer", (emailToSetToSocket)=>{
-		console.log(sockets.length);
 		socket.email = emailToSetToSocket;
-		console.log("socket.id = " + socket.id + " socket.email = " + socket.email);
+		var socketRepetido = 0;
 		sockets.forEach((sockets) =>{
-			console.log("Email: " + sockets.email);
+			if(sockets.email == emailToSetToSocket){
+				socketRepetido = 1;
+			}
 		});
-		sockets.push(socket);
-		console.log(sockets.length);
+		if(!socketRepetido){
+			sockets.push(socket);
+			console.log("Socket " + socket.email + " adicionado 'a lista => size = " + sockets.length);
+		}
+		socketRepetido = 0;
 	});
 
 	socket.on("userUpdated", (email)=>{
-		console.log("Searching for this email on the list of sockets");
-		sockets.forEach(sockets => {
-			if(email == sockets.email){
-				console.log("Foi encontrado o email: " + email);
-				io.to(`${sockets.id}`).emit("updateData"); //so' para quando o user esta a visualizar os seus movimentos
-				io.to(`${sockets.id}`).emit("notificationFromServer", "You have a new movement on your wallet!"); //sempre que o user estiver na aplicação
-				console.log("Mostrar notificação na aplicação");
-			}else{
-				socket.emit("sendEmail", email);
-				console.log("A enviar email para o user: " + email);
-			}
-		});
-	})
-	/*
-	socket.on("disconnect", (email)=>{
-		console.log(email);
-		console.log("Tamanho do array antes: " + sockets.lenght);
+		userEstaOnline = 0;
 		sockets.forEach(socket => {
 			if(email == socket.email){
-				sockets.pull(socket);
+				userEstaOnline = 1;
+				socketID = socket.id;
 			}
 		});
-		console.log("O user com o email " + email + " saiu da aplicação");
-		console.log("Tamanho do array depois: " + sockets.lenght);
+
+		if(userEstaOnline){
+			console.log("Foi encontrado o email: " + email);
+			io.to(`${socketID}`).emit("updateData"); //so' para quando o user esta a visualizar os seus movimentos
+			io.to(`${socketID}`).emit("notificationFromServer", "You have a new movement on your wallet!"); //sempre que o user estiver na aplicação
+			console.log("Mostrar notificação na aplicação");
+		}else{
+			var mailOptions = {
+				from: 'ipleiriadad@gmail.com',
+				to: email,
+				subject: 'Check out your wallet',
+				text: 'Check out you virtual wallet, you have a new movement!'
+			};
+	
+			transporter.sendMail(mailOptions, function(error, info){
+				if (error) {
+					console.log(error);
+				} else {
+					console.log('Email sent: ' + info.response);
+				}
+			});
+			console.log("A enviar email para o user: " + email);
+		}
+		userEstaOnline = 0;
 	})
-	*/
+	socket.on("disconnect", ()=>{
+		sockets.splice(socket.id);
+		console.log("Socket removido");
+	})
 });
